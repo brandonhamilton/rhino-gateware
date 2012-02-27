8eb9f1467162f83590208bde85833d795928a951
Alan Langman <alan.langman@gmail.com>
2012-02-27 16:53:15 +0200

    Added sweep rate to led test module

--- rhino/led/__init__.py
+++ rhino/led/__init__.py
@@ -12,7 +12,8 @@
 #
 #   Rhino platform - LED core
 #   Copyright (C) 2012 Brandon Hamilton
-#
+#   Copyrigth (C) 2012 Alan Langman
+#:
 #   This file is part of rhino-tools.
 #
 #   rhino-tools is free software: you can redistribute it and/or modify
@@ -30,17 +31,28 @@
 
 from migen.fhdl.structure import *
 from migen.bank import description
+import math
 
 class LED:
-
     COUNT = 8
 
-    def __init__(self):
-        self.led_register = Signal(BV(LED.COUNT), reset=0b10101010)
-        self.led_sync = [self.led_register.eq((self.led_register >> 1) | (self.led_register << 1 & 0b1000000))]
-    
+    def __init__(self,clk_freq=100e6,sweep_freq=2):
+        self.CLK_DIVIDE_COUNT   = int(clk_freq/sweep_freq);
+        self.CLK_DIVIDE_COUNT_N = int(math.ceil(math.log(self.CLK_DIVIDE_COUNT,2)))
+        self.led_register = Signal(BV(LED.COUNT), reset = 0b10101010)
+        self.clk_count = Signal(BV(self.CLK_DIVIDE_COUNT_N), reset = self.CLK_DIVIDE_COUNT)
+        self.led_sync = [
+                           If(self.clk_count == Constant(0,BV(self. CLK_DIVIDE_COUNT_N)),
+                              self.clk_count.eq(self.CLK_DIVIDE_COUNT),
+                              self.led_register.eq((self.led_register >> 1) | (self.led_register << 1 & 0b1000000))
+                             ).Else(
+                              self.clk_count.eq(self.clk_count-1)
+                                   )
+                        ] 
+   
     def get_fragment(self):
 
         # Return fragment with combinatoral and synchronous lists here
         return Fragment(sync=self.led_sync, pads={self.led_register})
 
+       

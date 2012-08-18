05f8b894da19f6e1c25728c567a49c8179727425
Sebastien Bourdeauducq <sebastien@milkymist.org>
2012-08-18 18:31:17 +0200

    platform: add FMC150 signals

--- platform/rhino/platform.py
+++ platform/rhino/platform.py
@@ -40,6 +40,50 @@ PLATFORM_RESOURCES = [
 	("gpmc_dmareq_n", 1, Pins("T26"), IOStandard("LVCMOS33")), # nCS3
 	("gpmc_dmareq_n", 2, Pins("V24"), IOStandard("LVCMOS33")), # nCS4
 	("gpmc_dmareq_n", 3, Pins("V26"), IOStandard("LVCMOS33")), # nCS5
+	
+	# FMC150
+	("fmc150_ctrl", 0,
+		Subsignal("sclk", Pins("AE5")),
+		Subsignal("sdata", Pins("AF5")),
+		
+		Subsignal("adc_sdo", Pins("U13")),
+		Subsignal("adc_en_n", Pins("AA15")),
+		Subsignal("adc_reset", Pins("V13")),
+		
+		Subsignal("cdce_sdo", Pins("AA8")),
+		Subsignal("cdce_en_n", Pins("Y9")),
+		Subsignal("cdce_reset_n", Pins("AB7")),
+		Subsignal("cdce_pd_n", Pins("AC6")),
+		Subsignal("cdce_pll_status", Pins("W7")),
+		Subsignal("cdce_ref_en", Pins("W8")),
+		
+		Subsignal("dac_sdo", Pins("W9")),
+		Subsignal("dac_en_n", Pins("W10")),
+		Subsignal("dac_txenable", Pins("AB15")),
+		
+		Subsignal("mon_sdo", Pins("AC5")),
+		Subsignal("mon_en_n", Pins("AD6")),
+		Subsignal("mon_reset_n", Pins("AF6")),
+		Subsignal("mon_int_n", Pins("AD5"))
+	),
+	("fmc150_dac", 0,
+		Subsignal("dat_p", Pins("AA10", "AA9", "V11", "Y11", "W14", "Y12", "AD14", "AE13")),
+		Subsignal("dat_n", Pins("AB11", "AB9", "V10", "AA11", "Y13", "AA12", "AF14", "AF13")),
+		Subsignal("frame_p", Pins("AB13")),
+		Subsignal("frame_n", Pins("AA13")),
+		Subsignal("clk_p", Pins("V12")),
+		Subsignal("clk_n", Pins("W12"))
+	),
+	("fmc150_adc", 0,
+		Subsignal("dat_a_p", Pins("AB14", "Y21", "W20", "AB22", "V18", "W17", "AA21")),
+		Subsignal("dat_a_n", Pins("AC14", "AA22", "Y20", "AC22", "W19", "W18", "AB21")),
+		Subsignal("dat_b_p", Pins("Y17", "U15", "AA19", "W16", "AA18", "Y15", "V14")),
+		Subsignal("dat_b_n", Pins("AA17", "V16", "AB19", "Y16", "AB17", "AA16", "V15")),
+		Subsignal("clk_p", Pins("AE15")),
+		Subsignal("clk_n", Pins("AF15"))
+	),
+	("fmc150_clk_to_fpga", 0, Pins("W24")),
+	("fmc150_ext_trigger", 0, Pins("U26")),
 ]
 
 CSR_BASE = 0x08000000

a78e868ce0416c8052c3d48d95c5303822807525
Sebastien Bourdeauducq <sebastien@milkymist.org>
2012-09-21 22:16:16 +0200

    library/led_controller: add led blinker

--- library/led_controller.py
+++ library/led_controller.py
@@ -6,3 +6,18 @@ class LedController(GPIO):
 		signals = [(baseapp.constraints.request(led_name), OUTPUT, "led"+str(i))
 			for i in range(count)]
 		super().__init__(baseapp, csr_name, UID_LED_CONTROLLER, signals)
+
+class LedBlinker:
+	def __init__(self, baseapp, divbits=26, led_name="user_led"):
+		self._led = baseapp.constraints.request(led_name)
+		self._divbits = divbits
+	
+	def get_fragment(self):
+		counter = Signal(BV(self._divbits))
+		comb = [
+			self._led.eq(counter[self._divbits-1])
+		]
+		sync = [
+			counter.eq(counter + 1)
+		]
+		return Fragment(comb, sync)

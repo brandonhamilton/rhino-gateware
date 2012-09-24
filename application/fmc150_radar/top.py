d6d1fdf20c8d5f75ca48a93a5414f343e2608da9
Sebastien Bourdeauducq <sebastien@milkymist.org>
2012-09-25 00:40:17 +0200

    application/fmc150_radar: waveform generator with DAC connection

--- application/fmc150_radar/top.py
+++ application/fmc150_radar/top.py
@@ -1,10 +1,34 @@
+from migen.flow.network import *
+from migen.actorlib.structuring import Cast
+
+from library.uid import UID_WAVEFORM_GENERATOR
 from library.crg import CRGFMC150
 from library.led_controller import LedBlinker, LedController
 from library.fmc150_controller import FMC150Controller
+from library.waveform_generator import WaveformGenerator
+from library.fmc150_data import DAC
+
+class FullWaveformGenerator(CompositeActor):
+	def __init__(self, baseapp):
+		dac_pins = baseapp.constraints.request("fmc150_dac")
+		width = 2*len(dac_pins.dat_p)
+		
+		wg = ActorNode(WaveformGenerator(1024, 4*width))
+		dac = ActorNode(DAC(dac_pins, baseapp.crg.io8x_strb))
+		cast = ActorNode(Cast(wg.actor.token("sample").layout(),
+			dac.actor.token("samples").layout()))
+		
+		baseapp.csrs.request("wg", UID_WAVEFORM_GENERATOR, *wg.actor.get_registers())
+		
+		g = DataFlowGraph()
+		g.add_connection(wg, cast)
+		g.add_connection(cast, dac)
+		super().__init__(g)
 
 COMPONENTS = [
 	CRGFMC150,
 	LedBlinker,
 	(LedController, {"count": 4}),
-	FMC150Controller
+	FMC150Controller,
+	FullWaveformGenerator
 ]

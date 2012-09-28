a14858d5be4d92c8eee1de0609740484fa49d18f
Sebastien Bourdeauducq <sebastien@milkymist.org>
2012-09-28 16:01:52 +0200

    Make 245MHz DAC sample rate the default

--- application/fmc150_radar/top.py
+++ application/fmc150_radar/top.py
@@ -6,15 +6,15 @@ from library.crg import CRGFMC150
 from library.led_controller import LedBlinker, LedController
 from library.fmc150_controller import FMC150Controller
 from library.waveform_generator import WaveformGenerator
-from library.fmc150_data import DAC
+from library.fmc150_data import DAC2X
 
 class FullWaveformGenerator(CompositeActor):
 	def __init__(self, baseapp):
 		dac_pins = baseapp.constraints.request("fmc150_dac")
 		width = 2*len(dac_pins.dat_p)
 		
-		wg = ActorNode(WaveformGenerator(1024, 2*width))
-		dac = ActorNode(DAC(dac_pins, baseapp.crg.dacio_strb))
+		wg = ActorNode(WaveformGenerator(1024, 4*width))
+		dac = ActorNode(DAC2X(dac_pins, baseapp.crg.dacio_strb))
 		cast = ActorNode(Cast(wg.actor.token("sample").layout(),
 			dac.actor.token("samples").layout()))
 		
@@ -27,7 +27,7 @@ class FullWaveformGenerator(CompositeActor):
 		super().__init__(g)
 
 COMPONENTS = [
-	CRGFMC150,
+	(CRGFMC150, {"double_dac": True}),
 	LedBlinker,
 	(LedController, {"count": 4}),
 	FMC150Controller,

e68eab902c5d5a906516ae1ecf8ef0bd32d58c40
Sebastien Bourdeauducq <sebastien@milkymist.org>
2012-12-14 23:56:45 +0100

    RFMD ISMM driver

--- library/rf_drivers.py
+++ library/rf_drivers.py
@@ -1,78 +1,69 @@
 from migen.fhdl.structure import *
+from migen.fhdl.tools import bitreverse
 from migen.flow.actor import *
 from migen.bank.description import *
 from migen.corelogic.fsm import FSM
 
-# 6-bit RF digital attenuator
-class PE43602Driver(Actor):
-	def __init__(self, cycle_bits=8):
+class SerialDataWriter:
+	def __init__(self, cycle_bits, data_bits, extra_fsm_states=[]):
+		self.cycle_bits = cycle_bits
+		self.data_bits = data_bits
+		
+		# I/O signals
 		self.d = Signal()
 		self.clk = Signal()
-		self.le = Signal()
 		
-		self._cycle_bits = cycle_bits
+		# control signals
+		self.pds = Signal()
+		self.pdi = Signal(self.data_bits)
 		
-		self._pos_end_cycle = RegisterField("pos_end_cycle", self._cycle_bits, reset=20)
-		self._pos_data = RegisterField("pos_data", self._cycle_bits, reset=0)
-		self._pos_le_high = RegisterField("pos_le_high", self._cycle_bits, reset=5)
-		self._pos_le_low = RegisterField("pos_le_low", self._cycle_bits, reset=15)
+		self.eoc = Signal()
+		self.ev_clk_high = Signal()
+		self.ev_clk_low = Signal()
+		self.ev_data = Signal()
 		
-		super().__init__(("attn", Sink, [("attn", 8)]))
+		# FSM
+		fsm_states = ["WAIT_DATA", "TRANSFER_DATA"] + extra_fsm_states
+		self.fsm = FSM(*fsm_states)
+		self.end_action = [self.fsm.next_state(self.fsm.WAIT_DATA)]
+		
+		# registers
+		self._pos_end_cycle = RegisterField("pos_end_cycle", self.cycle_bits, reset=20)
+		self._pos_data = RegisterField("pos_data", self.cycle_bits, reset=0)
 	
 	def get_registers(self):
-		return [self._pos_end_cycle, self._pos_data, self._pos_le_high, self._pos_le_low]
+		return [self._pos_end_cycle, self._pos_data]
 	
 	def get_fragment(self):
 		# cycle counter and events
-		cycle_counter = Signal(self._cycle_bits)
+		cycle_counter = Signal(self.cycle_bits)
 		cycle_counter_reset = Signal()
-		eoc = Signal()
 		comb = [
-			eoc.eq(cycle_counter == self._pos_end_cycle.field.r)
+			self.eoc.eq(cycle_counter == self._pos_end_cycle.field.r)
 		]
 		sync = [
-			If(eoc | cycle_counter_reset,
+			If(self.eoc | cycle_counter_reset,
 				cycle_counter.eq(0)
 			).Else(
 				cycle_counter.eq(cycle_counter + 1)
 			)
 		]
 		
-		ev_clk_high = Signal()
-		ev_clk_low = Signal()
-		ev_data = Signal()
 		comb += [
-			ev_clk_high.eq(cycle_counter == (self._pos_end_cycle.field.r >> 1)),
-			ev_clk_low.eq(cycle_counter == self._pos_end_cycle.field.r),
-			ev_data.eq(cycle_counter == self._pos_data.field.r)
-		]
-		
-		le_counter = Signal(self._cycle_bits)
-		le_counter_reset = Signal()
-		sync += [
-			If(le_counter_reset,
-				le_counter.eq(0)
-			).Else(
-				le_counter.eq(le_counter + 1)
-			)
-		]
-		
-		ev_le_high = Signal()
-		ev_le_low = Signal()
-		comb += [
-			ev_le_high.eq(le_counter == self._pos_le_high.field.r),
-			ev_le_low.eq(le_counter == self._pos_le_low.field.r)
+			self.ev_clk_high.eq(cycle_counter == (self._pos_end_cycle.field.r >> 1)),
+			self.ev_clk_low.eq(cycle_counter == self._pos_end_cycle.field.r),
+			self.ev_data.eq(cycle_counter == self._pos_data.field.r)
 		]
 		
 		# data
-		sr = Signal(8)
+		sr = Signal(self.data_bits)
 		sr_load = Signal()
 		sr_shift = Signal()
-		remaining_data = Signal(4)
+		remaining_data = Signal(max=self.data_bits+1)
 		sync += [
 			If(sr_load,
-				sr.eq(self.token("attn").attn),
-				remaining_data.eq(8)
+				sr.eq(self.pdi),
+				remaining_data.eq(self.data_bits)
 			).Elif(sr_shift,
 				sr.eq(sr[1:]),
 				self.d.eq(sr[0]),
@@ -93,6 +84,67 @@ class PE43602Driver(Actor):
 			self.clk.eq(clk_p)
 		]
 		
+		# control FSM
+		self.fsm.act(self.fsm.WAIT_DATA,
+			cycle_counter_reset.eq(1),
+			sr_load.eq(1),
+			If(self.pds,
+				self.fsm.next_state(self.fsm.TRANSFER_DATA)
+			)
+		)
+		self.fsm.act(self.fsm.TRANSFER_DATA,
+			clk_high.eq(self.ev_clk_high),
+			clk_low.eq(self.ev_clk_low),
+			sr_shift.eq(self.ev_data),
+			If(self.eoc & (remaining_data == 0),
+				*self.end_action
+			)
+		)
+		
+		return Fragment(comb, sync) + self.fsm.get_fragment()
+
+# 6-bit RF digital attenuator
+class PE43602Driver(Actor):
+	def __init__(self, cycle_bits=8):
+		self._sdw = SerialDataWriter(cycle_bits, 8, ["LE"])
+		self._sdw.end_action = [self._sdw.fsm.next_state(self._sdw.fsm.LE)]
+		
+		self.d = self._sdw.d
+		self.clk = self._sdw.clk
+		self.le = Signal()
+		
+		self._pos_le_high = RegisterField("pos_le_high", self._sdw.cycle_bits, reset=5)
+		self._pos_le_low = RegisterField("pos_le_low", self._sdw.cycle_bits, reset=15)
+		
+		super().__init__(("program", Sink, [("attn", 8)]))
+	
+	def get_registers(self):
+		return self._sdw.get_registers() + [self._pos_le_high, self._pos_le_low]
+	
+	def get_fragment(self):
+		comb = [
+			self._sdw.pds.eq(self.endpoints["program"].stb),
+			self._sdw.pdi.eq(self.token("program").attn)
+		]
+		
+		# LE counter
+		le_counter = Signal(self._sdw.cycle_bits)
+		le_counter_reset = Signal()
+		sync = [
+			If(le_counter_reset,
+				le_counter.eq(0)
+			).Else(
+				le_counter.eq(le_counter + 1)
+			)
+		]
+		
+		ev_le_high = Signal()
+		ev_le_low = Signal()
+		comb += [
+			ev_le_high.eq(le_counter == self._pos_le_high.field.r),
+			ev_le_low.eq(le_counter == self._pos_le_low.field.r)
+		]
+		
 		# LE
 		le_p = Signal()
 		le_high = Signal()
@@ -106,32 +158,18 @@ class PE43602Driver(Actor):
 			self.le.eq(le_p)
 		]
 		
-		# control FSM
-		fsm = FSM("WAIT_DATA", "TRANSFER_DATA", "LE")
-		
+		# complete FSM
+		fsm = self._sdw.fsm
 		fsm.act(fsm.WAIT_DATA,
-			cycle_counter_reset.eq(1),
 			le_counter_reset.eq(1),
-			sr_load.eq(1),
-			self.endpoints["attn"].ack.eq(1),
-			If(self.endpoints["attn"].stb,
-				fsm.next_state(fsm.TRANSFER_DATA)
-			)
+			self.endpoints["program"].ack.eq(1)
 		)
 		fsm.act(fsm.TRANSFER_DATA,
 			le_counter_reset.eq(1),
-			self.busy.eq(1),
-			clk_high.eq(ev_clk_high),
-			clk_low.eq(ev_clk_low),
-			sr_shift.eq(ev_data),
-			If(eoc & (remaining_data == 0),
-				fsm.next_state(fsm.LE)
-			)
+			self.busy.eq(1)
 		)
 		fsm.act(fsm.LE,
-			cycle_counter_reset.eq(1),
 			self.busy.eq(1),
-			cycle_counter_reset.eq(1),
 			le_high.eq(ev_le_high),
 			le_low.eq(ev_le_low),
 			If(ev_le_low,
@@ -139,20 +177,90 @@ class PE43602Driver(Actor):
 			)
 		)
 		
-		return Fragment(comb, sync) + fsm.get_fragment()
+		return Fragment(comb, sync) + self._sdw.get_fragment()
 
-# IQ Modulator with Synthesizer/VCO
-class RFMD2081Driver(Actor):
-	def __init__(self):
-		self.enx = Signal()
-		self.sclk = Signal()
+# RFMD's second-generation integrated synthesizer/mixer products, e.g.
+# RFMD2081 IQ Modulator with Synthesizer/VCO
+# RFFC5071 Wideband Synthesizer/VCO with Integrated Mixer
+class RFMDISMMDriver(Actor):
+	def __init__(self, cycle_bits=8):
+		self._sdw = SerialDataWriter(cycle_bits, 25, ["ENX_HI"])
+		self._sdw.end_action = [self._sdw.fsm.next_state(self._sdw.fsm.ENX_HI)]
+		
 		self.sdata = Signal()
+		self.sclk = Signal()
+		self.enx = Signal()
 		self.sdatao = Signal()
 		
-		super().__init__(("mod", Sink, [("addr", 7), ("data", 16)]))
+		# bitbang control
+		self._bb_enable = RegisterField("bb_enable")
+		self._bb_sdata = Field("sdata")
+		self._bb_sclk = Field("sclk")
+		self._bb_enx = Field("enx")
+		self._bb_out = RegisterFields("bb_out", [self._bb_sdata, self._bb_enx, self._bb_sclk])
+		self._bb_din = RegisterField("bb_din", access_dev=WRITE_ONLY, access_bus=READ_ONLY)
+		
+		super().__init__(("program", Sink, [("addr", 7), ("data", 16)]))
 	
 	def get_registers(self):
-		return []
+		return [self._bb_enable, self._bb_out, self._bb_in] + self._sdw.get_registers()
 	
 	def get_fragment(self):
-		return Fragment([self.endpoints["mod"].ack.eq(1)])
+		# ENX
+		enx = Signal(reset=1)
+		enx_p = Signal()
+		enx_high = Signal()
+		enx_low = Signal()
+		sync = [
+			If(enx_high,
+				enx_p.eq(1)
+			).Elif(enx_low,
+				enx_p.eq(0)
+			),
+			enx.eq(enx_p)
+		]
+		
+		# bitbang
+		comb = [
+			If(self._bb_enable.field.r,
+				self.sdata.eq(self._bb_sdata.r),
+				self.sclk.eq(self._bb_sclk.r),
+				self.enx.eq(self._bb_enx.r)
+			).Else(
+				self.sdata.eq(self._sdw.d),
+				self.sclk.eq(self._sdw.clk),
+				self.enx.eq(enx)
+			)
+		]
+		
+		# parallel data interface
+		word = Signal(25)
+		comb += [
+			self._sdw.pds.eq(self.endpoints["program"].stb),
+			word.eq(Cat(self.token("program").data, self.token("program").addr)),
+			self._sdw.pdi.eq(bitreverse(word))
+		]
+		
+		# complete FSM
+		fsm = self._sdw.fsm
+		fsm.act(fsm.WAIT_DATA,
+			self.endpoints["program"].ack.eq(1)
+		)
+		fsm.act(fsm.TRANSFER_DATA,
+			If(self._sdw.ev_data,
+				# ENX shares the data timing
+				enx_low.eq(1)
+			),
+			self.busy.eq(1)
+		)
+		fsm.act(fsm.ENX_HI,
+			If(self._sdw.ev_data,
+				enx_high.eq(1)
+			),
+			If(self._sdw.eoc,
+				fsm.next_state(fsm.WAIT_DATA)
+			),
+			self.busy.eq(1)
+		)
+		
+		return Fragment(comb, sync) + self._sdw.get_fragment()
---
+++ sim/rfmd_ismm.py
@@ -0,0 +1,29 @@
+from migen.flow.network import *
+from migen.flow.transactions import *
+from migen.actorlib.sim import *
+from migen.sim.generic import Simulator, TopLevel
+from migen.sim.icarus import Runner
+
+from library.rf_drivers import *
+
+class DataGen(SimActor):
+	def __init__(self):
+		def data_gen():
+			yield Token("ismm", {"addr": 0b1010010, "data": 0})
+			for i in range(16):
+				yield Token("ismm", {"addr": 0b1010101, "data": 1 << i})
+		super().__init__(data_gen(),
+			("ismm", Source, [("addr", 7), ("data", 16)]))
+
+def main():
+	g = DataFlowGraph()
+	g.add_connection(DataGen(), RFMDISMMDriver())
+	c = CompositeActor(g)
+	
+	def end_simulation(s):
+		s.interrupt = s.cycle_counter > 5 and not s.rd(c.busy)
+	f = c.get_fragment() + Fragment(sim=[end_simulation])
+	sim = Simulator(f, Runner(), TopLevel(vcd_name="rfmd_ismm.vcd"))
+	sim.run()
+
+main()

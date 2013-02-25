e9b2f8f0d664c528bf18e2dde1324e8b503ed7dd
Sebastien Bourdeauducq <sebastien@milkymist.org>
2013-02-25 23:28:41 +0100

    library: refactor waveform generator

--- library/ti_wave.py
+++ library/ti_wave.py
@@ -1,50 +1,116 @@
-from migen.flow.network import *
-from migen.flow.plumbing import Buffer
-from migen.actorlib.spi import Collector
-from migen.bank.description import regprefix
+from migen.fhdl.structure import *
+from migen.fhdl.specials import Memory
+from migen.bank.description import *
+from migen.genlib.cdc import MultiReg
 
 from library.uid import UID_WAVEFORM_GENERATOR, UID_WAVEFORM_COLLECTOR
-from library.waveform_generator import WaveformGenerator
-from library.ti_data import DAC, DAC2X, ADC
+from library.ti_io import DAC, DAC2X, ADC
 
-class FullWaveformGenerator(CompositeActor):
+class WaveformMemory:
+	def __init__(self, depth, width, spc):
+		self.depth = depth
+		self.width = width
+		self.spc = spc
+
+		self._mem = Memory(self.width, self.depth)
+		
+		# registers are in the system clock domain
+		self._r_play_en = RegisterField("playback_en")
+		self._r_size = RegisterField("size", bits_for(self.depth), reset=self.depth)
+		self._r_mult = RegisterField("mult", bits_for(self.depth), reset=1)
+
+		# register data transferred to signal clock domain
+		self._play_en = Signal()
+		self._size = Signal(bits_for(self.depth))
+		self._mult = Signal(bits_for(self.depth))
+		
+		# data interface, in signal clock domain
+		for i in range(self.spc):
+			name = "value" + str(i)
+			setattr(self, name, Signal(name=name))
+
+	def get_registers(self):
+		return [self._r_play_en, self._r_size, self._r_mult]
+
+	def get_memories(self):
+		return [self._mem]
+		
+	def get_fragment(self):
+		# transfer register data to signal clock domain
+		specials = {
+			MultiReg(self._r_play_en.field.r, "sys", self._play_en, "signal"),
+			MultiReg(self._r_size.field.r, "sys", self._size, "signal"),
+			MultiReg(self._r_mult.field.r, "sys", self._mult, "signal"),
+		}
+
+		# memory
+		mem_ports = [self._mem.get_port(clock_domain="signal") 
+			for i in range(self.spc)]
+		specials.add(self._mem)
+		
+		# logic
+		sync = []
+		for n, port in enumerate(mem_ports):
+			v_mem_a = Signal(bits_for(self.depth-1)+1, variable=True)
+			sync += [
+				v_mem_a.eq(port.adr),
+				If(~self._play_en,
+					v_mem_a.eq(n*self._mult)
+				).Else(
+					v_mem_a.eq(v_mem_a + self.spc*self._mult)
+				),
+				If(v_mem_a >= self._size,
+					v_mem_a.eq(v_mem_a - self._size)
+				),
+				port.adr.eq(v_mem_a),
+
+				getattr(self, "value" + str(n)).eq(port.dat_r)
+			]
+		
+		return Fragment(sync={"signal": sync}, specials=specials)
+
+class WaveformGenerator:
 	def __init__(self, baseapp):
 		dac_pins = baseapp.mplat.request("ti_dac")
 		width = 2*len(dac_pins.dat_p)
 		
-		spc = 2 if baseapp.double_dac else 1
-		dac_class = DAC2X if baseapp.double_dac else DAC
+		self._double_dac = baseapp.double_dac
+		spc = 2 if self._double_dac else 1
+		dac_class = DAC2X if self._double_dac else DAC
 		
-		wg_i = WaveformGenerator(1024, width, spc)
-		wg_q = WaveformGenerator(1024, width, spc)
-		dac = dac_class(dac_pins, baseapp.crg.dacio_strb)
+		self._wg_i = WaveformMemory(1024, width, spc)
+		self._wg_q = WaveformMemory(1024, width, spc)
+		self._dac = dac_class(dac_pins, baseapp.crg.dacio_strb)
 
-		registers = regprefix("i_", wg_i.get_registers()) \
-			+ regprefix("q_", wg_q.get_registers()) \
-			+ dac.get_registers()
-		baseapp.csrs.request("wg", UID_WAVEFORM_GENERATOR, *registers)
-		
-		g = DataFlowGraph()
-		if baseapp.double_dac:
-			g.add_connection(wg_i, dac, sink_subr=["i0", "i1"])
-			g.add_connection(wg_q, dac, sink_subr=["q0", "q1"])
+		registers = regprefix("i_", self._wg_i.get_registers()) \
+			+ regprefix("q_", self._wg_q.get_registers()) \
+			+ self._dac.get_registers()
+		memories = memprefix("i_", self._wg_i.get_memories()) \
+			+ memprefix("q_", self._wg_q.get_memories())
+		baseapp.csrs.request("wg", UID_WAVEFORM_GENERATOR, *registers, memories=memories)
+	
+	def get_fragment(self):
+		if self._double_dac:
+			comb = [
+				self._dac.i0.eq(self._wg_i.value0),
+				self._dac.q0.eq(self._wg_q.value0),
+				self._dac.i1.eq(self._wg_i.value1),
+				self._dac.q1.eq(self._wg_q.value1)
+			]
 		else:
-			g.add_connection(wg_i, dac, sink_subr=["i"])
-			g.add_connection(wg_q, dac, sink_subr=["q"])
-		CompositeActor.__init__(self, g)
+			comb = [
+				self._dac.i.eq(self._wg_i.value0),
+				self._dac.q.eq(self._wg_q.value0)
+			]
+		return self._wg_i.get_fragment() + self._wg_q.get_fragment() \
+			+ self._dac.get_fragment() + Fragment(comb)
 
-class FullWaveformCollector(CompositeActor):
+class WaveformCollector:
 	def __init__(self, baseapp):
-		adc_pins = baseapp.mplat.request("ti_adc")
-		width = 2*len(adc_pins.dat_a_p)
-		
-		adc = ADC(adc_pins)
-		buf = AbstractActor(Buffer)
-		wc = Collector(adc.token("samples").layout())
-		
-		baseapp.csrs.request("wc", UID_WAVEFORM_COLLECTOR, *wc.get_registers())
-		
-		g = DataFlowGraph()
-		g.add_connection(adc, buf)
-		g.add_connection(buf, wc)
-		CompositeActor.__init__(self, g)
+		#adc_pins = baseapp.mplat.request("ti_adc")
+		#adc = ADC(adc_pins)
+		#baseapp.csrs.request("wc", UID_WAVEFORM_COLLECTOR, *wc.get_registers())
+		pass # TODO
+
+	def get_fragment(self):
+		return Fragment()
